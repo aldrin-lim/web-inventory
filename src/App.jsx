741860e601b183634ee92a5d67fe7f01ca31ec3e
Aldrin Lim <aldrinlim81@gmail.com>
2023-11-15 22:26:43 +0800

    chore: test changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,10 @@ function App() {
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <p>Choo Choo! This is an example of a Vite + React app running on Railway.</p>
+        <p>Choo Choo! This is an example of a asdasdasdasdsadasdasdasdasdasdasd Vite + React app running on Railway.</p>
       </div>
     </>
   )
