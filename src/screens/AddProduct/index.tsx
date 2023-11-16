2c92689b43a3914f1e2a6ee066a71887b7b5346c
Aldrin Lim <aldrinlim81@gmail.com>
2023-11-16 08:08:35 +0800

    chore: Implement automated profit calculation

--- src/screens/AddProduct/index.tsx
+++ src/screens/AddProduct/index.tsx
@@ -36,7 +36,7 @@ const modalVariants: Variants = {
 }
 
 function calculateProfit(cost: number, price: number): number {
-  return price - cost;
+  return price - cost
 }
 
 const AddProductComponent = () => {
@@ -66,16 +66,10 @@ const AddProductComponent = () => {
     })
   }
 
-  useEffect(() => {
-    if (productDetails.price && productDetails.cost) {
-      
-    }
-  }, [productDetails.price, productDetails.cost])
-
   return (
     <div className="section relative flex flex-col gap-4 pt-0">
       <Formik initialValues={productDetails} onSubmit={() => {}}>
-        {({ setFieldValue }) => {
+        {({ setFieldValue, values }) => {
           return (
             <>
               <Toolbar
@@ -113,7 +107,7 @@ const AddProductComponent = () => {
               </Field>
               <button
                 onClick={() => setActiveModal(AddProductModal.Description)}
-                className="btn btn-primary btn-ghost btn-xs -mt-[10px]  flex w-full flex-row justify-between p-0 normal-case"
+                className="btn btn-ghost btn-primary btn-xs -mt-[10px]  flex w-full flex-row justify-between p-0 normal-case"
               >
                 <span className="col-span-11 w-4/5 truncate overflow-ellipsis text-left text-gray-400">
                   {description || 'Add Description'}
@@ -121,7 +115,7 @@ const AddProductComponent = () => {
                 <ChevronRightIcon className="col-span-1 w-5" />
               </button>
               <div className="grid w-full grid-cols-3 grid-rows-1 gap-4">
-                <Field name="price">
+                {/* <Field name="price">
                   {({ field, meta }: FieldProps) => (
                     <div className="form-control w-full">
                       <label className="label">
@@ -140,25 +134,58 @@ const AddProductComponent = () => {
                       <p className="form-control-error">{meta.error}&nbsp;</p>
                     </div>
                   )}
+                </Field> */}
+
+                <Field name="price">
+                  {({ field }: FieldProps) => (
+                    <div className="form-control w-full">
+                      <label className="label">
+                        <span className="label-text text-xs">Price</span>
+                      </label>
+                      <div className="join">
+                        <div className="indicator">
+                          <button className="btn disabled join-item px-2 text-gray-500">
+                            ₱
+                          </button>
+                        </div>
+                        <input
+                          {...field}
+                          type="number"
+                          className="input join-item input-bordered w-full pl-2"
+                          placeholder="Price"
+                          onChange={(e) => {
+                            setProductValue('price', e.target.value)
+                            setFieldValue('price', e.target.value)
+                          }}
+                        />
+                      </div>
+                    </div>
+                  )}
                 </Field>
 
                 <Field name="cost">
-                  {({ field, meta }: FieldProps) => (
+                  {({ field }: FieldProps) => (
                     <div className="form-control w-full">
                       <label className="label">
                         <span className="label-text text-xs">Cost</span>
                       </label>
-                      <input
-                        {...field}
-                        type="number"
-                        placeholder="Cost"
-                        className="input input-bordered w-full"
-                        onChange={(e) => {
-                          setProductValue('cost', e.target.value)
-                          setFieldValue('cost', e.target.value)
-                        }}
-                      />
-                      <p className="form-control-error">{meta.error} &nbsp;</p>
+                      <div className="join">
+                        <div className="indicator">
+                          <button className="btn disabled join-item px-2 text-gray-500">
+                            ₱
+                          </button>
+                        </div>
+                        <input
+                          {...field}
+                          type="number"
+                          className="input join-item input-bordered w-full pl-2"
+                          placeholder="Cost"
+                          onChange={(e) => {
+                            setProductValue('cost', e.target.value)
+                            setFieldValue('cost', e.target.value)
+                          }}
+                        />
+                      </div>
                     </div>
                   )}
                 </Field>
@@ -169,25 +196,36 @@ const AddProductComponent = () => {
                       <label className="label">
                         <span className="label-text text-xs">Profit</span>
                       </label>
-                      <input
-                        {...field}
-                        type="number"
-                        placeholder="Profit"
-                        className="input input-bordered w-full"
-                        onChange={(e) => {
-                          setProductValue('profit', e.target.value)
-                          setFieldValue('price', e.target.value)
-                        }}
-                      />
+                      <div className="join">
+                        <div className="indicator">
+                          <button className="btn disabled join-item px-2 text-gray-500">
+                            ₱
+                          </button>
+                        </div>
+                        <input
+                          {...field}
+                          disabled
+                          type="number"
+                          className="input join-item input-bordered w-full pl-2"
+                          placeholder="Profit"
+                          defaultValue={0}
+                          value={
+                            values.price &&
+                            values.cost &&
+                            Number(values.price) - Number(values.cost)
+                          }
+                        />
+                      </div>
                     </div>
                   )}
                 </Field>
               </div>
+
               <ImageUpload
                 onChange={(images) => setProductValue('images', images)}
               />
               <button
-                className="btn btn-primary btn-ghost btn-outline flex w-full flex-row justify-between"
+                className="btn btn-ghost btn-outline btn-primary flex w-full flex-row justify-between"
                 onClick={() => setActiveModal(AddProductModal.Detail)}
               >
                 <div className="flex flex-row items-center gap-1">
@@ -205,7 +243,7 @@ const AddProductComponent = () => {
       <AnimatePresence>
         <motion.div
           className={[
-            'absolute left-0 right-0 bg-base-100 h-full pt-0 section',
+            'section absolute left-0 right-0 h-full bg-base-100 pt-0',
             activeModal === AddProductModal.None ? 'hidden' : '',
           ].join(' ')}
           variants={modalVariants}
