e5bc68e6fea86433783cb973a266cb50cf0d6877
Aldrin Lim <aldrinlim81@gmail.com>
2023-11-20 20:02:07 +0800

    chore: truncate text

--- src/screens/ProductMenu/components/ProductCard.tsx
+++ src/screens/ProductMenu/components/ProductCard.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react'
 import { PhotoIcon } from '@heroicons/react/24/outline'
+import MiddleTruncatedText from 'components/MiddleTruncatedText'
 
 type ProductCardProps = {
   name: string
@@ -9,15 +11,30 @@ type ProductCardProps = {
 
 const ProductCard = (props: ProductCardProps) => {
   const { name, status, quantity, image } = props
+
+  // State to track if the image has failed to load
+  const [isImageBroken, setIsImageBroken] = useState(false)
+
+  // Function to call when the image fails to load
+  const handleImageError = () => {
+    setIsImageBroken(true)
+  }
+
   return (
     <div className="card card-compact w-[155px] border border-gray-300 bg-base-100">
       <figure className="h-[155px] w-[155px] bg-gray-300">
-        {image && <img src="" alt="Product picture" />}
-        {!image && <PhotoIcon className="w-24 text-gray-400" />}
+        {/* Show image or PhotoIcon based on image load status */}
+        {image && !isImageBroken ? (
+          <img src={image} alt="Product picture" onError={handleImageError} />
+        ) : (
+          <PhotoIcon className="w-24 text-gray-400" />
+        )}
       </figure>
-      <div></div>
       <div className="card-body flex flex-col gap-0 !py-2 text-left">
-        <h2 className="card-title text-base">{name}</h2>
+        <h2 className="card-title text-sm">
+          <MiddleTruncatedText text={name} maxLength={15} />
+        </h2>
+
         <div className="flex flex-row gap-1 text-xs">
           <span>{status || 'Active'}</span> • <span>{quantity} available</span>
         </div>
