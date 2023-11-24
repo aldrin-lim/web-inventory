0f6f082854400cdf1413021cbc1ddffd734508c3
Aldrin Lim <aldrinlim81@gmail.com>
2023-11-24 18:58:18 +0800

    chore: settings layout

--- src/screens/Settings/index.tsx
+++ src/screens/Settings/index.tsx
@@ -1,6 +1,10 @@
-import { ChevronLeftIcon } from '@heroicons/react/24/solid'
+import {
+  UserIcon,
+  BuildingStorefrontIcon,
+  ChevronRightIcon,
+  ArrowLeftOnRectangleIcon,
+} from '@heroicons/react/24/solid'
 import Toolbar from 'components/Layout/components/Toolbar'
-import ToolbarButton from 'components/Layout/components/Toolbar/components/ToolbarButton'
 import ToolbarTitle from 'components/Layout/components/Toolbar/components/ToolbarTitle'
 import { useNavigate } from 'react-router-dom'
 import { AppPath } from 'routes/AppRoutes.types'
@@ -8,7 +12,7 @@ import { AppPath } from 'routes/AppRoutes.types'
 const Settings = () => {
   const navigate = useNavigate()
   return (
-    <div className="section flex flex-col gap-4">
+    <div className="section flex h-full flex-grow flex-col gap-4">
       <Toolbar
         items={[
           <div key={0} />,
@@ -16,6 +20,37 @@ const Settings = () => {
           <div key={1} />,
         ]}
       />
+      <ul className="flex h-full flex-col">
+        <li>
+          <button
+            className="btn btn-ghost w-full justify-start px-1 "
+            onClick={() => navigate(AppPath.Profile)}
+          >
+            <UserIcon className="h-6 w-6" />
+            User Profile
+            <ChevronRightIcon className="ml-auto h-6 w-6 " />
+          </button>
+        </li>
+        <li className="flex-grow">
+          <button
+            className="btn btn-ghost w-full justify-start px-1"
+            onClick={() => navigate(AppPath.Profile)}
+          >
+            <BuildingStorefrontIcon className="h-6 w-6" />
+            Store
+            <ChevronRightIcon className="ml-auto h-6 w-6 " />
+          </button>
+        </li>
+        <li>
+          <button
+            className="btn btn-ghost mb-20 mt-auto w-full justify-start px-1"
+            onClick={() => navigate(AppPath.Profile)}
+          >
+            <ArrowLeftOnRectangleIcon className="h-6 w-6" />
+            Sign Out
+          </button>
+        </li>
+      </ul>
     </div>
   )
 }
