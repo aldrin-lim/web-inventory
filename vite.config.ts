b779139f5e687a154d9e10991508abfe545d0aee
Aldrin Lim <aldrinlim81@gmail.com>
2023-12-13 21:15:36 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,17 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
-
 export const hash = Math.floor(Math.random() * 90000) + 1000000
 
 // https://vitejs.dev/config/
 export default mergeConfig(viteConfig, {
   plugins: [react(), tsconfigPaths()],
   test: {},
+  define: {
+    // By default, Vite doesn't include shims for NodeJS/
+    // necessary for segment analytics lib to work
+    global: {},
+  },
   build: {
     rollupOptions: {
       output: {
