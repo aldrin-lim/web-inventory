1a1359971bef0c9da7f14de7f7e1a399fc72bd99
Aldrin Lim <aldrinlim81@gmail.com>
2024-02-19 17:23:55 +0800

    chore: Redirect not found url to home

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import Home from 'screens/Home'
 import { AppPath } from './AppRoutes.types'
 import ProtectedRoute from './ProtectedRoutes'
@@ -59,51 +59,7 @@ const AppRoutes = () => (
         element={<ProtectedRoute element={<Profile />} />}
       />
     </Route>
-    {/* <Route path="/" element={<ProtectedRoute element={<Layout />} />}>
-      <Route index element={<ProductMenu />} />
-      <Route path={AppPath.Home} element={<Home />} />
-      <Route path={AppPath.Orders} element={<Orders />} />
-      <Route path={AppPath.Products} element={<ProductMenu />} />
-      <Route
-        path={AppPath.RecipeOverview}
-        element={<ProtectedRoute element={<RecipeOverview />} />}
-      />
-      <Route
-        path={AppPath.Settings}
-        element={<ProtectedRoute element={<Settings />} />}
-      />
-    </Route> */}
-    {/* <Route path={AppPath.Products} element={<Products />} /> */}
-    {/* <Route
-      path={AppPath.Profile}
-      element={<ProtectedRoute element={<Profile />} />}
-    />
-    
-    <Route
-      path={AppPath.Staff}
-      element={<ProtectedRoute element={<Staff />} />}
-    />
-    <Route
-      path={AppPath.AddProduct}
-      element={<ProtectedRoute element={<AddProduct />} />}
-    />
-    <Route
-      path={AppPath.AddRecipe}
-      element={<ProtectedRoute element={<AddRecipe />} />}
-    />
-    <Route
-      path={AppPath.ViewProduct}
-      element={<ProtectedRoute element={<ViewProduct />} />}
-    />
-    <Route
-      path={AppPath.ViewRecipe}
-      element={<ProtectedRoute element={<ViewRecipe />} />}
-    />
-
-    <Route
-      path={AppPath.Error}
-      element={<ProtectedRoute element={<Error />} />}
-    /> */}
+    <Route path="*" element={<Navigate to={AppPath.Root} />} />
     <Route
       path={AppPath.Error}
       element={<ProtectedRoute element={<Error />} />}
