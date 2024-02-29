91c6a8273fe105f411366e93915c4aa9b2c787bf
Aldrin Lim <aldrinlim81@gmail.com>
2024-02-29 22:56:12 +0800

    chore: Add sentry sourcemapping

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,21 @@ import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { mergeConfig } from 'vitest/config'
 import viteConfig from './vite.config'
+import { sentryVitePlugin } from '@sentry/vite-plugin'
+
 export const hash = Math.floor(Math.random() * 90000) + 1000000
 
 // https://vitejs.dev/config/
 export default mergeConfig(viteConfig, {
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    sentryVitePlugin({
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      org: 'qrafter',
+      project: 'web-inventory',
+    }),
+  ],
   test: {},
   define: process.env.NODE_ENV === 'development' ? { global: 'window' } : {},
   build: {
