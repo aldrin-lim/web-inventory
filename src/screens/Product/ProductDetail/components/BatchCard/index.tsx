afbe336dae2f5dcf97b6375a39377c3a91b1e267
Aldrin Lim <aldrinlim81@gmail.com>
2024-03-06 17:48:46 +0800

    chore: Disable past date

--- src/screens/Product/ProductDetail/components/BatchCard/index.tsx
+++ src/screens/Product/ProductDetail/components/BatchCard/index.tsx
@@ -243,6 +243,7 @@ const BatchCard = (props: BatchCardProps) => {
           </div>
           <div className={`ExpirationDatePicker flex flex-row gap-1`}>
             <DatePicker
+              disablePast
               disabled={mode === 'edit'}
               sx={{ width: '100%', ':disabled': { backgroundColor: '#000' } }}
               slotProps={{
