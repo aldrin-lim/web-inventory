80d9c9565bf7ded410f0a7cc595e007dffd8f0c1
Aldrin Lim <aldrinlim81@gmail.com>
2024-03-19 21:59:33 +0800

    chore: Analytics to run on prod only

--- src/util/analytics.ts
+++ src/util/analytics.ts
@@ -12,41 +12,44 @@ const {
 
 export const Analytics = {
   init: () => {
-    console.log('analytics initialized')
-    VITE_SENTRY_ENVIRONMENT &&
-      VITE_SENTRY_DSN &&
-      Sentry.init({
-        dsn: VITE_SENTRY_DSN,
-        environment: MODE,
-        integrations: [Sentry.browserTracingIntegration()],
-        tracesSampleRate: 1.0,
-      })
-    VITE_MIXPANEL_ID &&
-      mixpanel.init(VITE_MIXPANEL_ID, {
-        debug: MODE !== 'production',
-        track_pageview: true,
-        persistence: 'localStorage',
-        ignore_dnt: true,
-        api_transport: 'sendBeacon',
-      })
-    VITE_HOTJAR_ID && hotjar.initialize(parseInt(VITE_HOTJAR_ID), 6)
+    if (MODE === 'production') {
+      console.log('analytics initialized')
+      VITE_SENTRY_ENVIRONMENT &&
+        VITE_SENTRY_DSN &&
+        Sentry.init({
+          dsn: VITE_SENTRY_DSN,
+          environment: MODE,
+          integrations: [Sentry.browserTracingIntegration()],
+          tracesSampleRate: 1.0,
+        })
+      VITE_MIXPANEL_ID &&
+        mixpanel.init(VITE_MIXPANEL_ID, {
+          debug: MODE !== 'production',
+          track_pageview: true,
+          persistence: 'localStorage',
+          ignore_dnt: true,
+          api_transport: 'sendBeacon',
+        })
+      VITE_HOTJAR_ID && hotjar.initialize(parseInt(VITE_HOTJAR_ID), 6)
+    }
   },
   identify: (id: string) => {
-    console.log('user identified')
-    VITE_MIXPANEL_ID && mixpanel.identify(id)
-    VITE_HOTJAR_ID && hotjar.identify(id, {})
-    VITE_SENTRY_DSN && Sentry.setUser({ id })
+    if (MODE === 'production') {
+      console.log('user identified')
+      VITE_MIXPANEL_ID && mixpanel.identify(id)
+      VITE_HOTJAR_ID && hotjar.identify(id, {})
+      VITE_SENTRY_DSN && Sentry.setUser({ id })
+    }
   },
   alias: (id: string) => {
     VITE_MIXPANEL_ID && mixpanel.alias(id)
   },
   track: (name: string, props?: Dict) => {
-    if (MODE === 'production') {
-      VITE_MIXPANEL_ID && mixpanel.track(name, props)
-    }
+    VITE_MIXPANEL_ID && mixpanel.track(name, props)
+    VITE_HOTJAR_ID && hotjar.event(name)
   },
   trackUnhandledError: (error: unknown) => {
-    if (VITE_SENTRY_ENVIRONMENT && VITE_SENTRY_DSN) {
+    if (MODE === 'production' && VITE_SENTRY_ENVIRONMENT && VITE_SENTRY_DSN) {
       Sentry.captureException(error)
     }
   },
